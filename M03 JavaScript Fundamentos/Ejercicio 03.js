cc3683a8728e0cea67c57adfe24dfae1d828bb7b
ageiron <paolanlxix@gmail.com>
2023-07-06 23:09:59 -0300

    M03 JavaScript Fundamentos/

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,6 +5,10 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y) {
+      console.log(true);
+} else {
+   console.log(false);
 }
 
 function tienenMismaLongitud(str1, str2) {
